a83873f044d2de7e42343b1a5c59e8e765091327
swatijadhav <swati.jadhav@kiprosh.com>
2015-12-17 16:57:00 +0530

    changed initialization aproach

--- src/android/Splunkmint.java
+++ src/android/Splunkmint.java
@@ -15,37 +15,31 @@ import android.os.Bundle;
 
 public class Splunkmint extends CordovaPlugin {
 
-    private static final String MINT_META_NAME = "mint_apikey";
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
+  }
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        super.initialize(cordova, webView);
+  public boolean execute(final String action, JSONArray args, CallbackContext callbackContext) throws JSONException
+  {
+    String mintApiKey = "";
+    boolean mintKeyFound = false;
 
-        String mintApiKey = "";
-        boolean mintKeyFound = false;
-
-        // load flurry api key from manifest meta data
-        try {
-            ApplicationInfo ai = cordova.getActivity().getPackageManager().getApplicationInfo(
-                cordova.getActivity().getPackageName(),
-                PackageManager.GET_META_DATA);
-            Bundle bundle = ai.metaData;
-            mintApiKey = bundle.getString(MINT_META_NAME);
-            mintKeyFound = true;
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.e("MintPlugin", "Failed to load meta-data, NameNotFound: " + e.getMessage());
-        } catch (NullPointerException e) {
-            Log.e("MintPlugin", "Failed to load meta-data, NullPointer: " + e.getMessage());
-        } catch (Exception e) {
-            Log.e("MintPlugin", "MintPlugin Exception: " + e.getMessage());
-        }
-
-        if (mintKeyFound) {
-            Mint.initAndStartSession(cordova.getActivity(), mintApiKey);
-            Log.v("MintPlugin", "Mint initAndStartSession done");
-        }
-
-
+    // load flurry api key from manifest meta data
+    try {
+      mintApiKey = args[0];
+      mintKeyFound = true;
+    } catch (PackageManager.NameNotFoundException e) {
+      Log.e("MintPlugin", "Failed to load meta-data, NameNotFound: " + e.getMessage());
+    } catch (NullPointerException e) {
+      Log.e("MintPlugin", "Failed to load meta-data, NullPointer: " + e.getMessage());
+    } catch (Exception e) {
+      Log.e("MintPlugin", "MintPlugin Exception: " + e.getMessage());
     }
 
+    if (mintKeyFound) {
+      Mint.initAndStartSession(cordova.getActivity(), mintApiKey);
+      Log.v("MintPlugin", "Mint initAndStartSession done");
+    }
+  }
 }
--- www/splunkmint.js
+++ www/splunkmint.js
@@ -3,3 +3,21 @@
 */
 
 module.exports = function(){};
+
+
+var exec = require('cordova/exec');
+
+function Splunkmint() {}
+
+Splunkmint.prototype.initiate = function(key){
+
+ exec(function(result){
+    /*alert("OK" + reply);*/
+  },
+  function(result){
+    /*alert("Error" + reply);*/
+  },"Splunkmint",key,[]);
+}
+
+ var splunkMint = new Splunkmint();
+ module.exports = splunkMint;
